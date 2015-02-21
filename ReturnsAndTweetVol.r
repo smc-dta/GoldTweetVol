c729df095f7c835bd2a715368b05b69b2dd43c45
¨Shannon <smc@digitaltrendanalytics.com>
2015-02-20 23:32:17 -0800

    working on wtw function

--- ReturnsAndTweetVol.r
+++ ReturnsAndTweetVol.r
@@ -7,43 +7,51 @@
 #The "wtw" tweets are those sent on weekends and holidays.  
 tw <- gt[gt$date %in% p$date, ]
 wtw <- gt[!(gt$date %in% p$date), ]
-for(i in 1:nrow(wtw)){wtw$days[i] <- (wtw[i+1,1]-wtw[i,1
+#days = number of days between current date and date of next row
+for(i in 1:nrow(wtw)){wtw$days[i] <- (wtw[i+1,1]-wtw[i,1])}
 wtw$days <- as.numeric(wtw$days)
+#wtw[! wtw$days == 1 & ! wtw$days == 6, ] #will show exceptions
 
-#Run this to see exceptions
-#wtw[! wtw$days == 1 & ! wtw$days == 6, ]
+wtw[nrow(wtw),3] <- 1 #Eliminates NA in last row
+#correct for holidays
+for(i in 1:nrow(wtw)){
+  if (wtw$date[i] == as.Date("2012-12-26") | wtw$date[i] == as.Date("2013-12-29")){
+    wtw$days[i] <- 1
+  }
+}
 
-func1 <- function(days){
+wtw$DateNew <- lapply(wtw$days, func2)
+
+
+
+
+func1 <- function(){
   for(i in 1:nrow(wtw)){
     if(wtw$days[i] > 1){
       wtw$DateNew[i] <- wtw$date[i] + 1
     } else {
-    if (wtw$date[i] == as.Date("2012-12-26")| wtw$date[i] == as.Date("2013-12-29")){
-      wtw$DateNew[i] <- wtw[i,1] + 1
-    } else {
-      wtw$DateNew[i] <- wtw$date[i] + 2}
+      wtw$DateNew[i] <- wtw$date[i] + 2
     }
   }
-} 
+}
 
 func2 <- function(days){
-    if(wtw$days > 1){
-      wtw$DateNew <- wtw$date + 1
-    } else {
-      if (wtw$date == as.Date("2012-12-26")| wtw$date == as.Date("2013-12-29")){
-        wtw$DateNew <- wtw$date + 1
-      } else {
-        wtw$DateNew <- wtw$date + 2}
-    }
+  if(wtw$days[i] > 1){
+    wtw$DateNew[i] <- wtw$date[i] + 1
+  } else {
+    wtw$DateNew[i] <- wtw$date[i] + 2
   }
+}
 
-ddply(wtw, (.days) func1(days))
 
-wtwNew <- ddply(wtw,c(1,2),func1(days))
+
+
+wtw %>% select(days) %>  
   
-wtw %>% class(days)  
-  
-  
+iris %>% select(Sepal.Length:Petal.Width) %>%
+  mutate(sumVar = rowSums(.)) %>% left_join(iris)
+
+
 
 
 wtw[ ,4] <- as.Date(wtw$DateNew, origin = "1970-01-01")
